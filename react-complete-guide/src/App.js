d8a9557b32d29e11734e66b85ef7241ba140aae6
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-11 14:43:49 -0400

    Basic components + Basic JSX

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 class App extends Component {
   render() {
+    // This is the JSX version
+    // JSX is similar to HTML but it's not  
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
       </div>
     );
+    // Another way to do the same as above  
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));  
   }
 }
 
