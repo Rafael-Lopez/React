43b61cea5f73483a615422586f4c111d9c69307d
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-11 14:49:11 -0400

    JSX Restrictions

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,9 +6,12 @@ class App extends Component {
     // This is the JSX version
     // JSX is similar to HTML but it's not  
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
+        // Typically, you only want one root element per component. In this case, 'div' is the root element.
+        // In HTML, the original attribute is class, but since this is JSX, 'class' can't be used since it's a JS reserved word. Instead, we have to use className
+        <div className="App">
+            <h1>Hi, I'm a React App</h1>
+            <p>This is really working!</p>
+        </div>
     );
     // Another way to do the same as above  
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));  
