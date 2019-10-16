0eafc4111ac26d4caf019430b016605549e7fd46
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-16 12:07:51 -0400

    Working with the children property

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,7 +3,12 @@ import React from 'react';
 // 'props' is passed by default by React
 // It includes all the attributes you add to your component
 const person = (props) => {
-    return <p>I'm {props.name} and I am {props.age} years old!</p>;
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    );
 };
 
 export default person;
