e4e06af68846915485ac61054d5b652d956fe02c
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-16 12:20:50 -0400

    Working with 'state'

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    // This is the JSX version
-    // JSX is similar to HTML but it's not  
-    return (
-        // Typically, you only want one root element per component. In this case, 'div' is the root element.
-        // In HTML, the original attribute is class, but since this is JSX, 'class' can't be used since it's a JS reserved word. Instead, we have to use className
-        <div className="App">
-            <h1>Hi, I'm a React App</h1>
-            <p>This is really working!</p>
-            <Person name="Max" age="28" />
-            <Person name="Manu" age="29" >My hobbies: Racing</Person>
-        </div>
-    );
-    // Another way to do the same as above  
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));  
-  }
+    
+    // state is a special property for any React component. So you can only use this within classes that extend Component
+    // What is special about 'state' is that if something in it changes, then React will re-render the component automatically to display the new value
+    state = {
+        persons: [
+            { name: 'Max', age: 28 },
+            { name: 'Manu', age: 29}
+        ]
+    }
+    
+    render() {
+        // This is the JSX version
+        // JSX is similar to HTML but it's not  
+        return (
+            // Typically, you only want one root element per component. In this case, 'div' is the root element.
+            // In HTML, the original attribute is class, but since this is JSX, 'class' can't be used since it's a JS reserved word. Instead, we have to use className
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button>Switch name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Racing</Person>
+            </div>
+        );
+        // Another way to do the same as above  
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));  
+    }
 }
 
 export default App;
