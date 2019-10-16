75da285718265087d011d0b18ae9c12f150bfe29
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-16 13:47:30 -0400

    Adding styling with Stylesheets

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css';
 
 // 'props' is passed by default by React
 // It includes all the attributes you add to your component
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click} >I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
