5ad58798dd1a50af07dc6efcff73679dcf73aaa6
Rafael-Lopez <rafael.lopez@bbd.ca>
2019-10-18 11:45:53 -0400

    Outputting lists

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -57,15 +57,11 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div>
-                    <Person 
-                        name={this.state.persons[0].name} 
-                        age={this.state.persons[0].age} />
-                    <Person 
-                        name={this.state.persons[1].name} 
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, 'Maximilian!')}
-                        changed={this.nameChangeHandler}>My hobbies: Racing
-                    </Person>
+                    { this.state.persons.map( (person) => {
+                        return <Person 
+                            name={person.name} 
+                            age={person.age} />
+                    } ) }
                 </div>
             );    
         }
