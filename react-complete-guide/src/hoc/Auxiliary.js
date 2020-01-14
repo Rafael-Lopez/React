9c613692b39b31427ddbe5c3b9f43931e783d14c
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-14 16:16:29 -0500

    Rendering adjacent JSX elements

---
+++ react-complete-guide/src/hoc/Auxiliary.js
@@ -0,0 +1,4 @@
+// props.childern is a special property that simply outputs whatever gets entered between the opening and closing tag of this component. Ex: <Auxiliary> ... </Auxiliary>
+const auxiliary = props => props.children;
+
+export default auxiliary;
