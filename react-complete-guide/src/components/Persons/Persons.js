ee82dde92fa7504ca3edc04678c1bd949776fc98
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-14 15:55:05 -0500

    PureComponent instead of shouldComponentUpdate

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
     
 //    static getDerivedStateFromProps(props, state) {
 //        console.log('[Persons.js] getDerivedStateFromProps');
@@ -9,10 +9,10 @@ class Persons extends Component {
 //    }
     
     // Decide whether to continue or not. Do not cause side-effects
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('[Persons.js] shouldComponentUpdate');
-        return true;
-    }
+    //shouldComponentUpdate(nextProps, nextState) {
+    //    console.log('[Persons.js] shouldComponentUpdate');
+    //    return true;
+    //}
     
     // Last-minute DOM ops. Do not cause side-effects.
     getSnapshotBeforeUpdate(prevProps, prevState) {
