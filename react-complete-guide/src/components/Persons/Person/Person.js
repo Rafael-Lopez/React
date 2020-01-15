9324c49e85ce2a7c43407146a89e4b0b0d36a521
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-15 10:10:42 -0500

    Using Refs

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,6 +6,17 @@ import withClass from '../../../hoc/withClass';
 // 'props' is passed by default by React
 // It includes all the attributes you add to your component
 class Person extends Component {
+    
+    constructor() {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+    
+    componentDidMount() {
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+    
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -13,6 +24,8 @@ class Person extends Component {
                 <p onClick={this.props.click} >I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
+                    //ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name}
