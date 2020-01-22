68066ea60100e23bbecd12f88bbb1c13d8051879
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-22 12:28:39 -0500

    Improving performance

--- burger-basics/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-basics/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxiliar from '../../../hoc/Auxiliar';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
-        return <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-               </li>;
-    });
+//This could be a functional component since Modal (the wrapping component)
+//is already taking care of unnecessary re-renders
+class OrderSummary extends Component {
 
-    return (
-        <Auxiliar>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Auxiliar>
-    );
-};
+    componentWillUpdate() {
+        console.log("Order Summary will update");
+    }
 
-export default orderSummary;
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+            return <li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+            </li>;
+        });
+
+        return (
+            <Auxiliar>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Auxiliar>
+        );
+    }
+}
+
+export default OrderSummary;
