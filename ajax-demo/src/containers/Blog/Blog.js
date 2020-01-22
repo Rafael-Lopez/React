6b89bc0eb88ce5a725a193c6544764f8a1d65ad8
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-22 13:58:31 -0500

    Creating an HTTP request to GET data

--- ajax-demo/src/containers/Blog/Blog.js
+++ ajax-demo/src/containers/Blog/Blog.js
@@ -4,8 +4,20 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+
+    componentDidMount() {
+
+        //GET returns a Promise object. Once the promise is fulfilled, the THEN
+        //part gets executed. Here's where you can manipulate the result
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then( response => {
+                console.log(response);
+            });
+    }
+
     render () {
         return (
             <div>
