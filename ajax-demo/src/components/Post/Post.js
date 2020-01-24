7fbef2731b185857b76f2085ec070af9764c8c92
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-24 11:47:06 -0500

    The withRouter HOC and Route props

--- ajax-demo/src/components/Post/Post.js
+++ ajax-demo/src/components/Post/Post.js
@@ -1,14 +1,22 @@
 import React from 'react';
-
+import {withRouter} from "react-router-dom";
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post" onClick={props.clicked}>
-        <h1>{props.title}</h1>
-        <div className="Info">
-            <div className="Author">{props.author}</div>
-        </div>
-    </article>
-);
+//The "withRouter" HOC we make this component Router-awared
+//This means it will have access to the React-Router properties (history, match, etc)
+//when rendered inside a component rendered by Route. "Posts" in this case.
+//Otherwise, those properties are not available in our case.
+const post = (props) => {
+    console.log(props);
 
-export default post;
+    return (
+        <article className="Post" onClick={props.clicked}>
+            <h1>{props.title}</h1>
+            <div className="Info">
+                <div className="Author">{props.author}</div>
+            </div>
+        </article>
+    );
+};
+
+export default withRouter(post);
