00922602c0402d396be1bfa7198cb30e81b96c50
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-24 11:33:52 -0500

    Using LINK to switch pages

--- ajax-demo/src/containers/Blog/Blog.js
+++ ajax-demo/src/containers/Blog/Blog.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import Posts from '../../containers/Blog/Posts/Posts'
 import NewPost from "./NewPost/NewPost";
-import {Route} from "react-router";
+import {Route, Link} from "react-router-dom";
 import './Blog.css';
 
 class Blog extends Component {
 
     render () {
+        //If you use a regular <a> tag for links, you will trigger a full update for the app
+        //Instead, use Link which is the primary way to allow users to navigate around your application
+        //Link will render a fully accessible anchor tag with the proper href.
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
