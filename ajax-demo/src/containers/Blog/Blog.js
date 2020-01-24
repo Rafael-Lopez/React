7d7c6cebe98797a08f03036bb3fa59595ca4ec66
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-24 11:59:07 -0500

    ABSOLUTE vs RELATIVE paths

--- ajax-demo/src/containers/Blog/Blog.js
+++ ajax-demo/src/containers/Blog/Blog.js
@@ -10,6 +10,17 @@ class Blog extends Component {
         //If you use a regular <a> tag for links, you will trigger a full update for the app
         //Instead, use Link which is the primary way to allow users to navigate around your application
         //Link will render a fully accessible anchor tag with the proper href.
+
+        /*ABSOLUTE & RELATIVE paths
+        ABSOLUTE: if your domain is example.com, then when you are at example.com/posts and in Link you
+        set 'to="/new-post"', your path will be example.com/new-post. The "/posts" part is removed. You
+        will always be re-directed to the root, which is example.com
+
+        RELATIVE: if your domain is example.com, then when you are at example.com/posts and in Link you
+        set 'to="/new-post"', your path will be example.com/posts/new-post.
+
+        By default, "to" is always treated as an absolute path. If you want to use relative paths, then
+        you can use this.props.match.url which is the currently loaded path and append the new path*/
         return (
             <div className="Blog">
                 <header>
