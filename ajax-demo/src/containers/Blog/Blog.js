ebc756f1fdc2fa1718421f48303299fcf7466ef7
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-24 11:11:39 -0500

    Preparing the project for Routing

--- ajax-demo/src/containers/Blog/Blog.js
+++ ajax-demo/src/containers/Blog/Blog.js
@@ -1,53 +1,11 @@
 import React, { Component } from 'react';
+import Posts from '../../containers/Blog/Posts/Posts'
 
-import Post from '../../components/Post/Post';
 import './Blog.css';
-import axios from 'axios';
 
 class Blog extends Component {
 
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount() {
-
-        //GET returns a Promise object. Once the promise is fulfilled, the THEN
-        //part gets executed. Here's where you can manipulate the result
-        axios.get('/posts')
-            .then( response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                   return {
-                        ...post,
-                       author: 'Max'
-                   };
-                });
-                this.setState( {posts: updatedPosts} );
-            })
-            .catch( error => {
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if(!this.state.error) {
-            posts = this.state.posts.map( post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />
-            });
-        }
-
         return (
             <div className="Blog">
                 <header>
@@ -58,9 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <Posts />
             </div>
         );
     }
