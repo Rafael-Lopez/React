fb1ad6ad7452cc0d9dfcb892d2d91fe14102b48f
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-27 11:37:02 -0500

    Router and Server Deployment

--- ajax-demo/src/App.js
+++ ajax-demo/src/App.js
@@ -6,6 +6,10 @@ class App extends Component {
   render() {
       return (
           <BrowserRouter>
+              {/*When hosting your application, you need to be aware of things you need to do for deployment*/}
+              {/*1) Make the server always go to index.html*/}
+              {/*2) If your app lives in example.com, you're good to go. If it's something like example.com/myApp, then see below*/}
+              {/*<BrowserRouter basename={'/myApp'}>*/}
               <div className="App">
                   <Blog />
               </div>
