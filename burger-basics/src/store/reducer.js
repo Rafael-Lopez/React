77e7845746486111259290b129e93dd138c499ed
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-29 11:24:17 -0500

    BURGUER BUILDER: Installing Redux and React Redux

---
+++ burger-basics/src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from '../store/actions';
+
+const initialState = {
+    ingredients: null,
+    totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer;
