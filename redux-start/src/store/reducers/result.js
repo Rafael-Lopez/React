6085f364d9c6f301a3eba1c1497fc55488c64029
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-01-29 16:04:08 -0500

    REDUX: Using utility functions

--- redux-start/src/store/reducers/result.js
+++ redux-start/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
     results: []
@@ -8,19 +9,12 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                // concat() returns a new Array + the element being concatenated. Therefore, we are still updating the state immutably
-                // push() does not create a new Array. Therefore, it's not immutably
-                results: state.results.concat({id: new Date(), value: action.result})
-            };
+            // concat() returns a new Array + the element being concatenated. Therefore, we are still updating the state immutably
+            // push() does not create a new Array. Therefore, it's not immutably
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
         case actionTypes.DELETE_RESULT:
             const updatedArray = state.results.filter(result => result.id !== action.resultElId );
-
-            return {
-                ...state,
-                results: updatedArray
-            };
+            return updateObject(state, {results: updatedArray});
     }
 
     return state;
