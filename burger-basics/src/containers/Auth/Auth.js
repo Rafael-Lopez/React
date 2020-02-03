f98648ef521e1c9a1b0ff4903fc3a8fe73a67905
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-02-03 16:37:57 -0500

    BURGER BUILDER: Redirecting the User to the Checkout page

--- burger-basics/src/containers/Auth/Auth.js
+++ burger-basics/src/containers/Auth/Auth.js
@@ -30,6 +30,12 @@ class Auth extends Component {
         isSignUp: true
     }
 
+    componentDidMount() {
+        if(!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+            this.props.onSetAuthRedirectPath();
+        }
+    }
+
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.controls
@@ -85,7 +91,7 @@ class Auth extends Component {
 
         let authRedirect = null;
         if(this.props.isAuthenticated) {
-            authRedirect = <Redirect to="/"/>;
+            authRedirect = <Redirect to={this.props.authRedirectPath}/>;
         }
 
         return (
@@ -108,13 +114,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAuthenticated: state.auth.token !== null
+        isAuthenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
 
