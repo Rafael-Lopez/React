06973d9a8fdbf00eb669d47d4fbca359c214e968
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-02-03 10:06:00 -0500

    BURGER BUILDER: Working on Order Actions

--- burger-basics/src/containers/Checkout/ContactData/ContactData.js
+++ burger-basics/src/containers/Checkout/ContactData/ContactData.js
@@ -61,8 +61,7 @@ class ContactData extends Component {
                 },
                 value: 'fastest'
             }
-        },
-        loading: false
+        }
     }
 
     orderHandler = (event) => {
@@ -118,7 +117,7 @@ class ContactData extends Component {
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner/>
         }
 
@@ -134,13 +133,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     };
 };
 
