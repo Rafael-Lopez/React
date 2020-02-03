c791f363f478834e7815f2937dcce9d52c75c3ec
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-02-03 09:54:10 -0500

    BURGER BUILDER: The Order Reducer

---
+++ burger-basics/src/store/reducers/order.js
@@ -0,0 +1,31 @@
+import * as actionTypes from '../actions/actionsTypes';
+
+const initialState = {
+    orders: [],
+    loading: false
+};
+
+const reducer = (state = initialState, action) => {
+        switch (action) {
+            case actionTypes.PURCHASE_BURGER_SUCCESS:
+                const newOrder = {
+                    ...action.orderData,
+                    id: action.orderId
+                };
+
+                return {
+                    ...state,
+                    loading: false,
+                    orders: state.orders.concat()
+                };
+            case actionTypes.PURCHASE_BURGER_FAIL:
+                return {
+                    ...state,
+                    loading: false
+                };
+            default:
+                return state;
+        }
+};
+
+export default reducer;
