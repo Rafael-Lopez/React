a770864459e0fe032a15fab2dc66dbf9f2ab7dbf
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-02-03 14:45:41 -0500

    BURGER BUILDER: Getting a Token from the Backend

--- burger-basics/src/store/actions/auth.js
+++ burger-basics/src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionsTypes';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -20,8 +21,22 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (mail, password) => {
+export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCLZ0kCUMVX1NZvG1b-njFpWFmlK2E3IIM', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch(authFail(error));
+            });
     };
 };
