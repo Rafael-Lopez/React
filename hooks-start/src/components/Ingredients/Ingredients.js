6ddb4a5178523acc6ecbce16050b3538531572c8
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-02-04 16:23:41 -0500

    REACT HOOKS: useEffect & Loading Data

--- hooks-start/src/components/Ingredients/Ingredients.js
+++ hooks-start/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -6,6 +6,27 @@ import IngredientList from "./IngredientList";
 
 function Ingredients() {
     const [userIngredients, setUserIngredients] = useState([]);
+
+    // useEffect() lets you perform side effects (data fetching, setting up a subscription, manually changing the DOM, etc)
+    // This runs after and for every render cycle
+    // You can tell React to skip applying an effect if certain values haven’t changed between re-renders. To do so,
+    // pass an array as an optional second argument to useEffect. However, you can also pass an empty array and useEffect()
+    // will act as componentDidMount: it runs only one (after the first render)
+    useEffect(() => {
+        fetch('https://react-hooks-update-7cad4.firebaseio.com/ingredients.json')
+            .then( response => response.json() )
+            .then( responseData => {
+                const loadedIngredients = [];
+                for (const key in responseData) {
+                    loadedIngredients.push({
+                        id: key,
+                        title: responseData[key].ingredient.title,
+                        amount: responseData[key].ingredient.amount
+                    });
+                }
+                setUserIngredients(loadedIngredients);
+            });
+    }, []);
 
     const addIngredientHandler = ingredient => {
         //We use fetch() this time instead of Axios, just to explore another solution
