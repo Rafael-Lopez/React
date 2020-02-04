8094de97e761349d32c7fa90d97fb8fe345c8657
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-02-04 15:58:13 -0500

    REACT HOOKS: Sending HTTP Requests

--- hooks-start/src/components/Ingredients/Ingredients.js
+++ hooks-start/src/components/Ingredients/Ingredients.js
@@ -7,11 +7,21 @@ import IngredientList from "./IngredientList";
 function Ingredients() {
     const [userIngredients, setUserIngredients] = useState([]);
 
-    const addIngredientHandler = ingredients => {
-        setUserIngredients(prevIngredients => [
-            ...prevIngredients,
-            { id: Math.random().toString(), ...ingredients }
-        ]);
+    const addIngredientHandler = ingredient => {
+        //We use fetch() this time instead of Axios, just to explore another solution
+        fetch('https://react-hooks-update-7cad4.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify({ingredient}),
+            headers: {'Content-Type': 'application/json'}
+        }).then( response => {
+            return response.json();
+        }).then( responseData => {
+            //We need a second then() block because json() returns a promise
+            setUserIngredients(prevIngredients => [
+                ...prevIngredients,
+                {id: responseData.name, ...ingredient}
+            ]);
+        });
     };
 
     const removeIngredientHandler = ingredientId => {
