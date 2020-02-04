f83fe987d2ab66dae6c40b51150c25f33ae2ac3b
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-02-04 14:58:26 -0500

    REACT HOOKS: More on useState() and State Updating

--- hooks-start/src/components/Ingredients/IngredientForm.js
+++ hooks-start/src/components/Ingredients/IngredientForm.js
@@ -17,11 +17,20 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
+            {/* Doing it this way, there's no guarantee that we will get the latest state as it may have not been committed yet by React
+                onChange={event => inputState[1]({title: event.target.value, amount: inputState[0].amount})}/>
+                Doing it as displayed below guarantees that we always get the latest state even if it hasn't been committed yet */}
             <input
                 type="text"
                 id="title"
                 value={inputState[0].title}
-                onChange={event => inputState[1]({title: event.target.value})}/>
+                onChange={ event => {
+                    const newTitle= event.target.value;
+                    inputState[1](prevInputState => ({
+                      title: newTitle,
+                      amount: prevInputState.amount
+                    }))
+                }}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
@@ -29,7 +38,13 @@ const IngredientForm = React.memo(props => {
                 type="number"
                 id="amount"
                 value={inputState[0].amount}
-                onChange={event => inputState[1]({amount: event.target.value})}/>
+                onChange={event => {
+                    const newAmount= event.target.value;
+                    inputState[1](prevInputState => ({
+                        amount: newAmount,
+                        title: prevInputState.title
+                    }))
+                }}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
