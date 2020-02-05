7cd7725ee72f5a9638cb79c5f3984cfa833ba462
Rafael-Lopez <rafael.lopez@bbd.ca>
2020-02-05 12:28:12 -0500

    REACT HOOKS: Loading errors and State Batching

--- hooks-start/src/components/Ingredients/Ingredients.js
+++ hooks-start/src/components/Ingredients/Ingredients.js
@@ -3,9 +3,12 @@ import React, {useState, useCallback} from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 
 function Ingredients() {
     const [userIngredients, setUserIngredients] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     // useCallback will return a memoized version of the callback that only changes if one of the dependencies has changed (this function is cached).
     // Basically, when this component is re-rendered, this specific function will not be re-created.
@@ -14,12 +17,14 @@ function Ingredients() {
     }, []);
 
     const addIngredientHandler = ingredient => {
+        setIsLoading(true);
         //We use fetch() this time instead of Axios, just to explore another solution
         fetch('https://react-hooks-update-7cad4.firebaseio.com/ingredients.json', {
             method: 'POST',
             body: JSON.stringify({ingredient}),
             headers: {'Content-Type': 'application/json'}
         }).then( response => {
+            setIsLoading(false);
             return response.json();
         }).then( responseData => {
             //We need a second then() block because json() returns a promise
@@ -31,16 +36,26 @@ function Ingredients() {
     };
 
     const removeIngredientHandler = ingredientId => {
+        setIsLoading(true);
         fetch(`https://react-hooks-update-7cad4.firebaseio.com/ingredients/${ingredientId}.json`, {
             method: 'DELETE'
         }).then( response => {
+            setIsLoading(false);
             setUserIngredients(prevIngredients => userIngredients.filter(ingredient => ingredient.id !== ingredientId));
+        }).catch(error => {
+            setError('Something went wrong!');
         });
     };
 
+    const clearError = () => {
+        setError(null);
+        setIsLoading(false);
+    }
+
     return (
         <div className="App">
-            <IngredientForm onAddIngredient={addIngredientHandler} />
+            {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+            <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
             <section>
             <Search onLoadIngredients={filteredIngredientsHandler}/>
