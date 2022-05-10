ab14fa75545108dded048bd2141eebd22aaa8282
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-10 17:01:07 -0400

    V2-Basics: Create basic component

---
+++ v2/react-basics/src/components/ExpenseItem.jsx
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>May 10, 2022</div>
+      <div>
+        <div>Car Insurance</div>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
