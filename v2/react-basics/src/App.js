6735dfd96c7d606080f7432d4e57399c5a052971
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-10 17:22:04 -0400

    V2-Basics: Pass data to components via props

--- v2/react-basics/src/App.js
+++ v2/react-basics/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2022, 5, 10)}
+        title={"Car Insurance"}
+        amount={294.67}
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2022, 5, 10)}
+        title={"Toilet Paper"}
+        amount={13.50}
+      ></ExpenseItem>
     </div>
   );
 }
