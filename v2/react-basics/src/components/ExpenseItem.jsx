f3671f30f3ce767ec96364cff62f9f9ecf4fb97b
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-10 17:11:54 -0400

    V2-Basics: Output dynamic data

--- v2/react-basics/src/components/ExpenseItem.jsx
+++ v2/react-basics/src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 5, 10);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   return (
-    <div className='expense-item'>
-      <div>May 10, 2022</div>
-      <div className='expense-item__description'>
-        <div>Car Insurance</div>
-        <div className='expense-item__price'>$294.67</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <div>{expenseTitle}</div>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
