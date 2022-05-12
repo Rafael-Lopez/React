d5630231d9cb977eeb7ac927fb70b6d820a25dbd
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-12 01:00:17 -0400

    V2-State & Events: Add two-way binding

--- v2/react-basics/src/components/NewExpense/ExpenseForm.jsx
+++ v2/react-basics/src/components/NewExpense/ExpenseForm.jsx
@@ -30,6 +30,10 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+
+    setNewTitle('');
+    setNewAmount('');
+    setNewDate('');
   };
 
   return (
@@ -37,7 +41,7 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={newTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -45,6 +49,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={newAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -54,6 +59,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={newDate}
             onChange={dateChangeHandler}
           />
         </div>
