d0fe34b49beb2e915933275cfd0af66a380bea42
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-11 22:20:52 -0400

    V2-Basics: Events and event handlers

--- v2/react-basics/src/components/Expenses/ExpenseItem.jsx
+++ v2/react-basics/src/components/Expenses/ExpenseItem.jsx
@@ -3,6 +3,10 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const clickHanlder = () => {
+    console.log('Clicked!!!');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHanlder}>Change Title</button>
     </Card>
   );
 }
