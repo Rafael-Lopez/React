35d7641777f3426714a818e8daa30b7a3502044c
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-11 23:23:56 -0400

    V2-State & Events: Add form inputs

--- v2/react-basics/src/App.js
+++ v2/react-basics/src/App.js
@@ -1,23 +1,24 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
     {
       date: new Date(2022, 5, 10),
       title: "Car Insurance",
-      amount: 294.67
+      amount: 294.67,
     },
     {
       date: new Date(2022, 5, 10),
       title: "Toilet Paper",
-      amount: 13.50
-    }
+      amount: 13.5,
+    },
   ];
 
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
