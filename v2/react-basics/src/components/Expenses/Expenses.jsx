9761eb7419d243384d7fb5734a4d2aca7d79f81b
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-13 14:07:57 -0400

    V2-State & Events: Assignment 2

--- v2/react-basics/src/components/Expenses/Expenses.jsx
+++ v2/react-basics/src/components/Expenses/Expenses.jsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2020");
+
+  const filterChangeHandler = filteredYear => {
+    setSelectedYear(filteredYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.title}
-          date={expense.date}
-          title={expense.title}
-          amount={expense.amount}
-        />
-      ))}
-    </Card>
+    <div>
+      <ExpensesFilter selected={selectedYear} onChangeFilter={filterChangeHandler} />
+      <Card className="expenses">
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.title}
+            date={expense.date}
+            title={expense.title}
+            amount={expense.amount}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 
