23aa30c3c75bda3632112488702c7e6afe83f8cf
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-14 16:38:48 -0400

    V2-Conditional rendering: Add conditional return statements

--- v2/react-basics/src/components/Expenses/Expenses.jsx
+++ v2/react-basics/src/components/Expenses/Expenses.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState("2020");
@@ -11,20 +12,9 @@ const Expenses = (props) => {
     setSelectedYear(filteredYear);
   };
 
-  const filteredExpenses = props.expenses.filter((expense) => expense.date.getFullYear().toString() === selectedYear);
-
-  let expensesContent = <p>No expenses found!</p>;
-  
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        date={expense.date}
-        title={expense.title}
-        amount={expense.amount}
-      />
-    ));
-  }
+  const filteredExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === selectedYear
+  );
 
   return (
     <div>
@@ -33,7 +23,7 @@ const Expenses = (props) => {
           selected={selectedYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
