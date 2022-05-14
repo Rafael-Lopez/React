c1e7cef7001ba84fc349111d2a33ec15887beb09
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-14 16:27:31 -0400

    V2-Conditional rendering: Output conditional content

--- v2/react-basics/src/components/Expenses/Expenses.jsx
+++ v2/react-basics/src/components/Expenses/Expenses.jsx
@@ -11,6 +11,21 @@ const Expenses = (props) => {
     setSelectedYear(filteredYear);
   };
 
+  const filteredExpenses = props.expenses.filter((expense) => expense.date.getFullYear().toString() === selectedYear);
+
+  let expensesContent = <p>No expenses found!</p>;
+  
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        date={expense.date}
+        title={expense.title}
+        amount={expense.amount}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,16 +33,7 @@ const Expenses = (props) => {
           selected={selectedYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.expenses
-          .filter((expense) => expense.date.getFullYear().toString() === selectedYear)
-          .map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              date={expense.date}
-              title={expense.title}
-              amount={expense.amount}
-            />
-          ))}
+        {expensesContent}
       </Card>
     </div>
   );
