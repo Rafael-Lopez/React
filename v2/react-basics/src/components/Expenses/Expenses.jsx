ed12804bcb887848be29d27a33af7edc31ead412
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-14 18:34:49 -0400

    V2-Conditional rendering: Add chart tracker

--- v2/react-basics/src/components/Expenses/Expenses.jsx
+++ v2/react-basics/src/components/Expenses/Expenses.jsx
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState("2020");
@@ -23,6 +24,7 @@ const Expenses = (props) => {
           selected={selectedYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
