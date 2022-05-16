b4f5d4755ff8f4150a69450541cc567e3c364a28
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-15 22:53:02 -0400

    V2-useEffect, useReducer, useContext: Use useEfect() & Dependencies

--- v2/useEffect-demo/src/App.js
+++ v2/useEffect-demo/src/App.js
@@ -7,6 +7,11 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // *** useEffect() ***
+  // - Empty array: acts like componentDidMount as in, it only runs once.
+  // - No second argument: acts as both componentDidMount and componentDidUpdate, as in it runs first on mount and then on every re-render.
+  // - Array as second argument with any value inside, eg , [variable1]: will only execute the code inside your useEffect hook ONCE on mount,
+  //   as well as whenever that particular variable (variable1) changes.
   useEffect(() => {
     const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
 
