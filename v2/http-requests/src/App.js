c9c49e4879556ebef9973b96883acb5d2d23dded
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-18 00:12:38 -0400

    V2-HTTP Requests: Sending a POST request

--- v2/http-requests/src/App.js
+++ v2/http-requests/src/App.js
@@ -48,9 +48,17 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  const addMovieHandler= movie => {
-    console.log(movie);
-  }; 
+  const addMovieHandler = async (movie) => {
+    const response = await fetch("https://SOME_URL", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json;
+    console.log(data);
+  };
 
   let content = <p>Found No Movies</p>;
 
