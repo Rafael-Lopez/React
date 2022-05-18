0ade7fb680b5be5f025956a9ea9982060c23c781
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-18 01:58:00 -0400

    V2-Redux: How to work with Redux state correctly

--- v2/redux/src/store/index.js
+++ v2/redux/src/store/index.js
@@ -3,31 +3,33 @@ import { createStore } from "redux";
 const initialState = { counter: 0, showCounter: true };
 
 const counterReducer = (state = initialState, action) => {
+  // When working with Reduc, you should never, super important, never mutate the state - the existing state.
+  // You should never change the existing state. Instead, always override it by returning a brand new state object.
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
-      showCounter: state.showCounter
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
-      showCounter: state.showCounter
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
-      showCounter: state.showCounter
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "toggle") {
     return {
       showCounter: !state.showCounter,
-      counter: state.counter
+      counter: state.counter,
     };
   }
 
