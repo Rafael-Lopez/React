cad05fcc5cb9c86e94c9bdce19c5fc7db447fb6c
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-17 23:11:34 -0400

    V2-HTTP Requests: Handle loading & data states

--- v2/http-requests/src/App.js
+++ v2/http-requests/src/App.js
@@ -5,13 +5,17 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // An async function is a function declared with the async keyword, and the await keyword is permitted within it. 
-  // The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding 
+  // An async function is a function declared with the async keyword, and the await keyword is permitted within it.
+  // The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding
   // the need to explicitly configure promise chains.
   async function fetchMoviesHandler() {
+    setIsLoading(true);
+
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
+
     const transformedMovies = data.results.map((movie) => {
       return {
         id: movie.episode_id,
@@ -22,6 +26,7 @@ function App() {
     });
 
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -30,7 +35,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading &&movies.length === 0 && <p>Found No Movies</p>}
+        {!isLoading &&  movies.length > 0 && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
