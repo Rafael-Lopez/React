2426bcf3fb358f5dc42ee12cc2e2d7ee531bf705
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-17 23:39:56 -0400

    V2-HTTP Requests: Using useEffect() for requests

--- v2/http-requests/src/App.js
+++ v2/http-requests/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -11,13 +11,13 @@ function App() {
   // An async function is a function declared with the async keyword, and the await keyword is permitted within it.
   // The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding
   // the need to explicitly configure promise chains.
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
       const response = await fetch("https://swapi.dev/api/films");
-      
+
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -38,7 +38,14 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  // We could omit having 'fetchMoviesHandler' as a dependency, and that would indeed achieve the result we want.
+  // But it could introduce subtle bugs, if our function would be using some external state. A better
+  // solution is to use the useCallback hook and wrap fetchMoviesHandler with that.
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found No Movies</p>;
 
@@ -46,12 +53,12 @@ function App() {
     content = <MoviesList movies={movies} />;
   }
 
-  if(error) {
-    content = <p>{error}</p>
+  if (error) {
+    content = <p>{error}</p>;
   }
 
-  if(isLoading) {
-    content = <p>Loading...</p>
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -59,9 +66,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
