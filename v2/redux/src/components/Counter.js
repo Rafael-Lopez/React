0f47a1766029fefca437d290eb9365d2063cf7f0
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-18 13:56:32 -0400

    V2-Redux: Migrating everything to Redux Toolkit

--- v2/redux/src/components/Counter.js
+++ v2/redux/src/components/Counter.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -12,19 +13,19 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- v2/redux/src/store/index.js
+++ v2/redux/src/store/index.js
@@ -15,7 +15,7 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.amount;
+      state.counter = state.counter + action.payload;
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
@@ -34,5 +34,13 @@ const store = configureStore({
   reducer: { counter: counterSlice.reducer }
 }); 
 */
+
+// To dispatch actions, createSlice() has got us covered. It automatically creates unique action identifiers for our different reducers.
+// To get hold of these action identifiers, we can do counterSlice.actions. This returns an object full of keys, where the the key names are:
+// increment, decrement, increase, and toggleCounter.
+// With this, we get methods created automatically by Redux Toolkit, which when called will create action objects for us.
+// Therefore, these methods are called ACTION CREATORS, and they will create action objects for us where these objects already have a type 
+// property with a unique identifier per action automatically created behind the scenes.
+export const counterActions = counterSlice.actions;
 
 export default store;
