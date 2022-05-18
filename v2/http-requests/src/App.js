f9c3134244f8885dd2a003c6cb102e79119560ad
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-17 23:25:46 -0400

    V2-HTTP Requests: Handle HTTP errors

--- v2/http-requests/src/App.js
+++ v2/http-requests/src/App.js
@@ -6,27 +6,52 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   // An async function is a function declared with the async keyword, and the await keyword is permitted within it.
   // The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding
   // the need to explicitly configure promise chains.
   async function fetchMoviesHandler() {
     setIsLoading(true);
+    setError(null);
 
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-    const transformedMovies = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      };
-    });
+      const data = await response.json();
 
-    setMovies(transformedMovies);
+      const transformedMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>Found No Movies</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if(error) {
+    content = <p>{error}</p>
+  }
+
+  if(isLoading) {
+    content = <p>Loading...</p>
   }
 
   return (
@@ -35,9 +60,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {isLoading && <p>Loading...</p>}
-        {!isLoading &&movies.length === 0 && <p>Found No Movies</p>}
-        {!isLoading &&  movies.length > 0 && <MoviesList movies={movies} />}
+        {content}
       </section>
     </React.Fragment>
   );
