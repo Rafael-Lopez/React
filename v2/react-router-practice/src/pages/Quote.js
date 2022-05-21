ba553ebc6dbb9cb02138df4d1cf2f8a987318211
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-21 02:54:02 -0400

    V2-React Router: Outputting data on the Details page

--- v2/react-router-practice/src/pages/Quote.js
+++ v2/react-router-practice/src/pages/Quote.js
@@ -1,15 +1,26 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun" },
+  { id: "q2", author: "Max", text: "Learning React is great" },
+];
 
 const Quote = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+      return <p>No quote found!</p>
+  }
+
   return (
     <Fragment>
-      <h1>Quote</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
