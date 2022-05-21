56f4bf8d9987ea375a6adf0b528ef251873a6da2
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-21 00:07:56 -0400

    V2-React Router: Using Switch and 'exact' to configure routes

--- v2/react-router/src/App.js
+++ v2/react-router/src/App.js
@@ -1,24 +1,39 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import MainHeader from "./components/MainHeader";
 import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
 import Welcome from "./pages/Welcome";
 
+/*
+  <Switch>: Renders the first child <Route> or <Redirect> that matches the location. Without it, all Routes that match (and 
+  by match we mean, all routes that start with the given path) will be rendered.
+*/
+
+/*
+  When wrapping our Routes with Switch, React router simply goes through your routes top to bottom. When it finds a match,
+  (if it finds a match), it will then stop, because of Switch, and will render that one route for which it did find a match.
+  It will not look at the other routes. 
+
+  In our example below, we can either use 'exact', or put the Route at the bottom.
+*/
+
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
