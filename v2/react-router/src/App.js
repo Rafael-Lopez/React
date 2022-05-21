01ac96ceda397511dbb9fc1d6d3e683ef9c9a08b
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-20 23:20:54 -0400

    V2-React Router: Extract Route Params

--- v2/react-router/src/App.js
+++ v2/react-router/src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path="/products">
           <Products />
         </Route>
-        <Route path="/products-detail/:productId">
+        <Route path="/product-detail/:productId">
           <ProductDetail />
         </Route>
       </main>
