c35f81ad0fcd566e27c5d09736eda866ed1ed167
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-21 00:17:33 -0400

    V2-React Router: Redirecting the user

--- v2/react-router/src/App.js
+++ v2/react-router/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import MainHeader from "./components/MainHeader";
 import Products from "./pages/Products";
@@ -24,6 +24,9 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
           <Route path="/welcome">
             <Welcome />
           </Route>
