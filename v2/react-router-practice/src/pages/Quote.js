29d1bca49c9a843e6e04caa1a677ad4f1eb8787d
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-21 17:05:34 -0400

    V2-React Router: More work with Nested Routes

--- v2/react-router-practice/src/pages/Quote.js
+++ v2/react-router-practice/src/pages/Quote.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
@@ -15,12 +15,19 @@ const Quote = () => {
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-      return <p>No quote found!</p>
+    return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
