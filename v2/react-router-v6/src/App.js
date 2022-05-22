0978bb349c56ee18840247216e643e9640783ba5
Rafael Lopez <lopez.rafael08@gmail.com>
2022-05-22 16:33:19 -0400

    V2-React Router: Upgrading to React Router v6 - Initial migration changes

---
+++ v2/react-router-v6/src/App.js
@@ -0,0 +1,35 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Welcome from "./pages/Welcome";
+
+function App() {
+
+  /*
+    React router v6 doesn't support 'exact' anymore.
+      // old - v5 <Route exact path="/" component={Home} />
+      // new - v6 <Route path="/" element={<Home />} />
+
+    As stated in their documentation:
+      You don't need to use an exact prop on <Route path="/"> anymore. This is because all paths match exactly by default. 
+      If you want to match more of the URL because you have child routes use a trailing * as in <Route path="users/*">.
+  */
+
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/welcome" />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
